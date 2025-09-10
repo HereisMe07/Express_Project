524d623107fbe71b218a73304c2997d263a4d225
IT_TEK_VITU\Nssf_User <tujames77@gmail.com>
2025-09-10 15:56:55 +0700

    connet to database

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,42 +1,46 @@
+//Wrap async
+const asyncHandler = require("express-async-handler");
+
 //@desc Get all contacts
 //@route Get /api/contacts
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc Create New contact
 //@route POST /api/contacts
 //@access public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     console.log("The request body is :", req.body);
     const { name, email, phone } = req.body;
     if (!name || !email || !phone) {
-        res.status(400)
+        res.status(400);
+        throw new Error("All fields are mandatory !")
     }
-    res.status(201).json({ message: "Creat contact"});
-};
+    res.status(201).json({ message: "Creat contact" });
+});
 
 //@desc Get single contact
 //@route GET /api/contacts/:id
 //@access public
-const getSingleContact = (req, res) => {
+const getSingleContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Get contact with id ${req.params.id}`});
-};
+});
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
     res.status(200).json({message:`Update contact with id ${req.params.id}`})
-};
+});
 
 //@desc Delete contact
 //@route Delete /api/contacts/:id
 //@access Public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({message: `Delete contact with id ${req.params.id}`})  
-};
+});
 
 
 module.exports = { getContact, createContact, getSingleContact, updateContact, deleteContact };
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // import express framework
 const express = require("express");
+const errorHandler = require("./middleware/errorhandle");
+const connectDb = require("./config/dbConnection");
 
 // load environment variables from .env file
 const dotenv = require("dotenv").config();
 
+connectDb();
 const app = express();
 
 // set port from .env or use 5000 by default
@@ -12,6 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 // load contact routes
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 // start server
 app.listen(port, () => {
